864e8c5365f4483c7782086a98cfba5fb0749d6d
mayurithatipamula <thatipamulamayuri@gmail.com>
2025-12-28 15:02:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Search from './components/search';
+
+function App() {
+  return (
+    <>
+
+      <Search />
+    </>
+  );
+}
+export default App;
